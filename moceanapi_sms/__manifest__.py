64eae2568a2cf585afe51a900cad328bbe59c8ec
koksianchong2 <ks_chong@workspace.mocean.com.my>
2024-12-09 16:07:37 +0800

    updated to version 18

--- moceanapi_sms/__manifest__.py
+++ moceanapi_sms/__manifest__.py
@@ -1,30 +1,34 @@
-# -*- coding: utf-8 -*-
 {
     "name": "MoceanAPI SMS",
-    "version": "18.0.0.0",
-    "depends": ["base","mail"],
-    "author": "MoceanAPI Developer",
-    "license" : "LGPL-3",
-    "images": [
-    ],
+    "version": "18.0",
+    "summary": "SMS integration with MoceanAPI",
     "description": """
-        To get started, go to our website https://moceanapi.com to register a new account to claim 20 free credit.
-
+        To get started, go to our website https://moceanapi.com to register a new account to claim 20 free credits.
     """,
+    "author": "MoceanAPI Developer",
     "website": "https://moceanapi.com",
+    "license": "LGPL-3",
     "category": "Tools",
-    "demo": [],
+    "depends": ["base", "mail", "web"],
     "data": [
-        "view/assets.xml",
         "security/security.xml",
         "security/ir.model.access.csv",
         "view/moceansms.xml",
-        "moceansms_data.xml"
+        "moceansms_data.xml",
     ],
-    "active": False,
+    "assets": {
+        "web.assets_backend": [
+            "moceanapi_sms/static/src/css/main.css",
+            "moceanapi_sms/static/src/css/cookieconsent.min.css",
+            "moceanapi_sms/static/src/js/cookieconsent.min.js",
+            "moceanapi_sms/static/src/js/yandex.js",
+        ],
+        'web.assets_qweb': [
+            'moceanapi_sms/static/src/xml/*.xml',
+        ],
+    },
+    "images": ["static/description/banner.png"],
     "installable": True,
     "application": True,
-    "images": ["static/description/banner.png"]
+    "auto_install": False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- moceanapi_sms/static/src/js/yandex.js
+++ moceanapi_sms/static/src/js/yandex.js
@@ -1,8 +1,12 @@
-jQuery(function($) {
+document.addEventListener('DOMContentLoaded', function() {
     var waitForElementToAppear = setInterval(function() {
-        if ($('.o_menu_brand').text() == 'MoceanAPI SMS') {
-            $('<div />').attr('class', 'mc-consent-overlay').appendTo("body");
-            $(".mc-consent-overlay").hide();
+        var menuBrandElement = document.querySelector('.o_menu_brand');
+        if (menuBrandElement && menuBrandElement.textContent === 'MoceanAPI SMS') {
+            var consentOverlay = document.createElement('div');
+            consentOverlay.className = 'mc-consent-overlay';
+            document.body.appendChild(consentOverlay);
+            consentOverlay.style.display = 'none';
+
             window.cookieconsent.initialise({
                 "revokeBtn": "<div class='cc-revoke cc-bottom cc-right cc-animate cc-color-override-688238583'>Data usage consent</div>",
                 "palette": {
@@ -24,33 +28,40 @@ jQuery(function($) {
                     "href": "https://moceanapi.com/legal/privacy"
                 },
                 onInitialise: function(status) {
-                    if(status == cookieconsent.status.allow) myScripts();
+                    if (status == cookieconsent.status.allow) myScripts();
                 },
                 onStatusChange: function(status) {
                     if (this.hasConsented()) myScripts();
                 },
                 onPopupOpen: function() {
-                    $(".mc-consent-overlay").show();
+                    consentOverlay.style.display = 'block';
                 },
                 onPopupClose: function() {
-                    $(".mc-consent-overlay").hide();
+                    consentOverlay.style.display = 'none';
                 }
             });
+            
             clearInterval(waitForElementToAppear);
         }
     }, 250);
-
 });
 
 function myScripts() {
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    (function(m, e, t, r, i, k, a) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0];
+        k.async = 1;
+        k.src = r;
+        a.parentNode.insertBefore(k, a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(89818200, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
+    ym(89818200, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true
+    });
 }
