eb52e9acde009926fb6d3487a822ed7c51544550
koksianchong2 <ks_chong@workspace.mocean.com.my>
2024-12-10 10:58:06 +0800

    Update __manifest__.py

--- moceanapi_sms/__manifest__.py
+++ moceanapi_sms/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "MoceanAPI SMS",
-    "version": "17.0.0.0",
+    "version": "18.0.0.0",
     "depends": ["base","mail"],
     "author": "MoceanAPI Developer",
     "license" : "LGPL-3",
